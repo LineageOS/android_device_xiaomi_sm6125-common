e795d13ab79fd5faefd26efaa999b6563a61987f
sudharani <sshakunt@codeaurora.org>
2021-09-29 16:48:59 +0530

    sm6125-common: Set manufacturer and soc properties
    Set soc model and manufacturer properties
    
    Change-Id: Ia75eb8862c7d879c40e64527d32d082bfa9b8e42

--- properties.mk
+++ properties.mk
@@ -215,6 +215,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.sensors.sta_detect=true \
     ro.vendor.sensors.mot_detect=true
 
+# SoC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.soc.manufacturer=Qualcomm \
+    ro.soc.model=SM6125
+
 # Time
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.delta_time.enable=true
