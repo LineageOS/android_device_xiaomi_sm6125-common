de2865b6795b63cc5a0dd2fda3635c60aa12f7f4
danascape <danascape@gmail.com>
2021-11-01 19:03:25 +0100

    sm6125-common: Build libavservices_minijail
    Change-Id: I43233e269bf1f40b288efa0a9160b0077e02efc4

--- common.mk
+++ common.mk
@@ -334,6 +334,8 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libavservices_minijail.vendor \
     libc2dcolorconvert \
     libextmedia_jni \
     libhypv_intercept \
