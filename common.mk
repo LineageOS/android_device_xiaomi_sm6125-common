2dd40053a0408b00d84b13c9978bcd99012dd7d2
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2022-04-26 15:05:39 +0200

    sm6125-common: display: add libdisplayconfig.qti to product packages
    Change-Id: Ic8a69bc5368f9d41c5958181ecb091e6d0d4ad05

--- common.mk
+++ common.mk
@@ -194,6 +194,7 @@ PRODUCT_PACKAGES += \
     hwcomposer.trinket \
     memtrack.trinket \
     libdisplayconfig \
+    libdisplayconfig.qti \
     libdisplayconfig.vendor \
     libtinyxml \
     libvulkan \
