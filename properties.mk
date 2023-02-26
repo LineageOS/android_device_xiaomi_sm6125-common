0fcc965e5be4897a449b8b6acfbc9b0ea4c3274d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-02-26 19:53:10 +0100

    sm6125-common: Disable QTI perf lock usage in camera HAL
     * our dummy qti-perfd-client doesnt play well with our
       camera hal and crashes everytime lens is switched
    
    11-26 00:15:37.902 16507 16507 F DEBUG   : pid: 15967, tid: 16032, name: HwBinder:15967_  >>> /vendor/bin/hw/android.hardware.camera.provider@2.4-service <<<
    11-26 00:15:37.902 16507 16507 F DEBUG   : uid: 1047
    11-26 00:15:37.902 16507 16507 F DEBUG   : signal 11 (SIGSEGV), code 1 (SEGV_MAPERR), fault addr 0x0
    11-26 00:15:37.902 16507 16507 F DEBUG   : Cause: null pointer dereference
    ...
    11-26 00:15:37.902 16507 16507 F DEBUG   : backtrace:
    11-26 00:15:37.902 16507 16507 F DEBUG   :       #00 pc 0000145e  /vendor/lib/libqti-perfd-client.so (perf_lock_acq+6) (BuildId: b9021283b270fb111fd8dfa2403d21fe)
    11-26 00:15:37.902 16507 16507 F DEBUG   :       #01 pc 000a1565  /vendor/lib/hw/camera.trinket.so (qcamera::QCameraPerfLock::acquirePerfLock(bool, unsigned int)+184) (BuildId: 4d6a6ba4dea7af2adad6d095fe545643)
    
    Change-Id: I8bf4332c47cd3bc78a1d59af9d9f02436e7bc218

--- properties.mk
+++ properties.mk
@@ -106,6 +106,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.camera.perflock.enable=0 \
     persist.vendor.camera.preview.ubwc=0 \
     vendor.video.disable.ubwc=1 \
     vidc.enc.dcvs.extra-buff-count=2
