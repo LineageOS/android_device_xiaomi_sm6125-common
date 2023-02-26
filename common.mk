40da88be28851e011dac79c4b8214186ed3f53af
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-02-26 19:40:42 +0100

    sm6125-common: Migrate to Xiaomi power AIDL HAL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- common.mk
+++ common.mk
@@ -392,8 +392,12 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power@1.2.vendor
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
