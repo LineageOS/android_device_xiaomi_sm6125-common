13fa662e14abdfda27130c0c108421644afce5a6
DarkJoker360 <simoespo159@gmail.com>
2023-02-26 17:53:06 +0100

    sm6125-common: Move privapp permissions to /system_ext
    Change-Id: Ie96694843eccdf4ec529d4561bd48e26b7d813a7

--- common.mk
+++ common.mk
@@ -39,7 +39,7 @@ QCOM_SOONG_NAMESPACE := $(LOCAL_PATH)
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
