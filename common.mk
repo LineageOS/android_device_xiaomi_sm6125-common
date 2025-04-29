a66bea73e6c55c8a444e2fd324256576117cfe7e
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:11:42 +0100

    sm6125-common: Set libperfmgr mode extension lib via soong config
    Change-Id: I39ac8867d98808832738f58eb6f50d456d6d34c4

--- common.mk
+++ common.mk
@@ -184,6 +184,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libperfmgr-ext-xiaomi)
+
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
