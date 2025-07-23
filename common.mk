56fccf8c35bd4ba007430c22ce353a5167652d6d
Skyblueborb <tomaszborbely0710@gmail.com>
2025-07-23 18:07:34 +0200

    Revert "sm6125-common: Build QTI Thermal AIDL HAL"
    This reverts commit 929b56e2e9e6dc7ca0217095f4636c4028c3e108.
    
    Reason: Temperatures reported are innacurate.
    Change-Id: I95ec0f9bd7c41e87c9b30f2bd2d13884e6874dbe

--- common.mk
+++ common.mk
@@ -253,10 +253,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.qti
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
