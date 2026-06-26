2f2ba2484b43ac28f7ad6752387bead12a617e36
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-26 01:19:38 -0400

    sm6125-common: fastbootd is now enabled by default
    * Why were we ever building a custom impl???
    
    Change-Id: Ieb54140550168f84ba4f067605fd6f7107979c31

--- common.mk
+++ common.mk
@@ -79,10 +79,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
-# fastbootd
-PRODUCT_PACKAGES += \
-    fastbootd
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi
